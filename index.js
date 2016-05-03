7873bee17aae254dd7b6ebe3ada07faea6ea0969
Haroen Viaene <hello@haroen.me>
2016-05-03 11:56:12 +0200

    connect

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const config = require('./config.json');
 const mailgun = require('mailgun-js')({apiKey: config.key, domain: config.domain});
 const Firebase = require('firebase');
 const base = new Firebase(config.firebase.url);
+
+base.authWithCustomToken(config.firebase.secret, function(error, authData) {
+  if(!error) {
+    console.log(authData);
+  }
+});
